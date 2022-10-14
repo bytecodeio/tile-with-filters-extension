618ad4616e45eb84b84ab79e77b6dc8a16b09f3a
seanp-bytecode <93162346+seanp-bytecode@users.noreply.github.com>
2022-10-14 11:17:40 -0700

    Update README.md

--- src/HelloWorldVisComponent.js
+++ src/HelloWorldVisComponent.js
@@ -17,56 +17,32 @@ import { Query, Visualization } from "@looker/visualizations";
 export const HelloWorldVisComponent = () => {
   const { core40SDK } = useContext(ExtensionContext);
 
-  // Enter a query value in the form
-  const queryTypeOptions = [
-    {
-      label: "Query",
-      value: "query",
-    },
-    {
-      label: "Dashboard",
-      value: "dashboard",
-    },
-  ];
-  const [selectedQueryType, setSelectedQueryType] = useState("query");
-  const [queryValue, setQueryValue] = useState("");
+  const [queryInputValue, setQueryInputValue] = useState("");
+  const [queryValue, setQueryValue] = useState();
 
   // Set the prop of the <Query /> component on submit
-  const [queryProp, setQueryProp] = useState();
-  const handleSubmit = () => {
-    setQueryProp({
-      [selectedQueryType]: queryValue,
-    });
-  };
+  const handleSubmit = () => setQueryValue(queryInputValue);
 
   return (
     <>
       <ComponentsProvider>
         <Heading>Looker Visualization Component</Heading>
         <Heading as="h4">
-          Enter a query slug or a dashboard ID and click submit to render a
-          visualization component.
+          Enter a query slug and click submit to render a visualization
+          component.
         </Heading>
 
-        <RadioGroup
-          value={selectedQueryType}
-          name="queryTypes"
-          options={queryTypeOptions}
-          onChange={setSelectedQueryType}
-        />
         <FieldText
           name="queryValue"
-          value={queryValue}
-          onChange={(e) => setQueryValue(e.target.value)}
-          label={`${
-            queryTypeOptions.find((t) => t.value === selectedQueryType).label
-          } ID`}
+          value={queryInputValue}
+          onChange={(e) => setQueryInputValue(e.target.value)}
+          label="Query ID"
         />
         <Button onClick={handleSubmit}>Submit</Button>
 
-        {queryProp && (
+        {queryValue && (
           <DataProvider sdk={core40SDK}>
-            <Query {...queryProp}>
+            <Query query={queryValue}>
               <Visualization />
             </Query>
           </DataProvider>
